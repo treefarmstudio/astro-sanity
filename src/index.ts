344e136fa95bcb52541cf0dc30d1a6232795edb2
Jaydan Urwin <jaydan@jaydanurwin.com>
2022-09-03 22:16:33 -0600

    better astro integration

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,30 @@
-export { default as sanityClient } from '@sanity/client';
+import sanityClient, { ClientConfig } from '@sanity/client';
 
 export { createImageBuilder } from './createImageBuilder';
 
 export { portableTextToHtml } from './portableTextToHtml';
 
+function initSanityClient(config: ClientConfig) {
+  const client = sanityClient(config);
+
+  globalThis.sanityClient = client;
+}
+
+export function useSanityClient() {
+  if (!globalThis.sanityClient) {
+    console.error('Sanity client has not been initialized correctly');
+  }
+
+  return globalThis.sanityClient;
+}
+
+export default function astroSanityIntegration(options: ClientConfig) {
+  return {
+    name: 'astro-sanity',
+    hooks: {
+      'astro:config:setup': () => {
+        initSanityClient(options);
+      },
+    },
+  };
+}

63fa6e6a518af3f49bb1a9a73b2ee4940c087673
Jaydan Urwin <jaydan@jaydanurwin.com>
2022-12-30 12:19:47 -0700

    ssr test

--- demo-ssr/astro.config.mjs
+++ demo-ssr/astro.config.mjs
@@ -7,5 +7,10 @@ import sanity from 'astro-sanity'
 export default defineConfig({
   output: 'server',
   adapter: vercel(),
-  integrations: [image()],
+  integrations: [image(), sanity({
+    projectId: '8hj1t7km',
+    dataset: 'production',
+    apiVersion: '2021-03-25',
+    useCdn: true
+  })],
 });
--- demo-ssr/src/sanity/api.js
+++ demo-ssr/src/sanity/api.js
@@ -1,8 +1,8 @@
-import { sanityClient } from './client.js';
+import { useSanityClient } from 'astro-sanity';
 import { groq } from 'astro-sanity';
 
 export async function getFirstBlogPost() {
   const query = groq`*[_type == "post" && _id == "0b3a877f-9ede-4360-a7b0-dee96107215e"][0]`;
-  const firstPost = await sanityClient.fetch(query);
+  const firstPost = await useSanityClient().fetch(query);
   return firstPost;
 }

f81b359f1cb4d63f9bfd004e3ac56f39db22115d
Jaydan Urwin <jaydan@jaydanurwin.com>
2022-12-30 08:06:02 -0700

    fixing workspaces

--- demo/src/sanity/api.js
+++ demo/src/sanity/api.js
@@ -1,5 +1,4 @@
-import { useSanityClient } from 'astro-sanity';
-import groq from groq
+import { useSanityClient, groq } from 'astro-sanity';
 
 export async function getFirstBlogPost() {
   const query = groq`*[_type == "post" && _id == "0b3a877f-9ede-4360-a7b0-dee96107215e"][0]`;

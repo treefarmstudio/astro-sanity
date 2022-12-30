2e09faabe51144407c53ed75b8a4ec763b048c4f
Jaydan Urwin <jaydan@jaydanurwin.com>
2022-12-30 07:57:35 -0700

    setting up npm workspaces

--- demo/src/sanity/api.js
+++ demo/src/sanity/api.js
@@ -1,7 +1,8 @@
 import { useSanityClient } from 'astro-sanity';
+import groq from groq
 
 export async function getFirstBlogPost() {
-  const query = `*[_type == "post" && _id == "0b3a877f-9ede-4360-a7b0-dee96107215e"][0]`;
+  const query = groq`*[_type == "post" && _id == "0b3a877f-9ede-4360-a7b0-dee96107215e"][0]`;
   const firstPost = await useSanityClient().fetch(query);
   return firstPost;
 }

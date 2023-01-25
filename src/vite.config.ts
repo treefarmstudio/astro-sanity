3bb76b28493389747afae0c28096f48e5d40e955
Grant Sander <gksander93@gmail.com>
2023-01-25 13:11:17 -0600

    Generate type defs

--- src/vite.config.ts
+++ src/vite.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from "vite";
 import path from "path";
+import dts from "vite-plugin-dts";
 
 const name = "astro-sanity";
 
 export default defineConfig(() => {
   return {
+    plugins: [dts()],
     build: {
       lib: {
         entry: path.resolve(__dirname, "index.ts"),

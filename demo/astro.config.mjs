7878fe9dfac2f82528da8500ca6ba8fcc3a94c57
Jaydan Urwin <jaydan@jaydanurwin.com>
2023-01-30 22:04:53 -0700

    astro upgrade to 2.0 on demos

--- demo/astro.config.mjs
+++ demo/astro.config.mjs
@@ -1,10 +1,9 @@
 import { defineConfig } from 'astro/config';
-
-import image from "@astrojs/image";
 import sanity from 'astro-sanity'
 // https://astro.build/config
 export default defineConfig({
-  integrations: [image(), sanity({
+  integrations: [
+    sanity({
     projectId: '8hj1t7km',
     dataset: 'production',
     apiVersion: '2021-03-25',

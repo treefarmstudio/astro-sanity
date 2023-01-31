d6787991b6da3f105db90c4911fb5a62e7239ea3
Jaydan Urwin <jaydan@jaydanurwin.com>
2023-01-30 22:06:39 -0700

    cleanup dead import

--- demo-ssr/astro.config.mjs
+++ demo-ssr/astro.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel/serverless';
-import image from "@astrojs/image";
 import sanity from 'astro-sanity'
 
 // https://astro.build/config

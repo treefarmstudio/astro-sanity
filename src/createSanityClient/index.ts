6296d7d0986c5a9137c51a42f5bd165d891105b7
Jaydan Urwin <jaydan@jaydanurwin.com>
2023-05-01 23:33:48 -0600

    export update and version bump

--- src/createSanityClient/index.ts
+++ src/createSanityClient/index.ts
@@ -1,5 +1,5 @@
-import sanityClient, { ClientConfig, SanityClient } from '@sanity/client';
+import { createClient, ClientConfig, SanityClient } from '@sanity/client';
 
 export function createSanityClient(config: ClientConfig): SanityClient {
-  return sanityClient(config);
+  return createClient(config);
 }
